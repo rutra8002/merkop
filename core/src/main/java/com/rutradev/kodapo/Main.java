82b341f2d038d301bc39dcd5231343454c161a06
rutrarch <arturkummer08@gmail.com>
2026-05-19 18:59:44 +0200

    init

---
+++ core/src/main/java/com/rutradev/kodapo/Main.java
@@ -0,0 +1,42 @@
+package com.rutradev.kodapo;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+public class Main extends ApplicationAdapter {
+    private SpriteBatch batch;
+    private Texture image;
+    private BitmapFont font;
+
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        image = new Texture("libgdx.png");
+        font = new BitmapFont();
+    }
+
+    @Override
+    public void render() {
+        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
+        batch.begin();
+        batch.draw(image, 140, 210);
+
+        int fps = Gdx.graphics.getFramesPerSecond();
+        font.draw(batch, "FPS: " + fps, 10, Gdx.graphics.getHeight() - 10);
+
+
+        batch.end();
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        image.dispose();
+    }
+}
